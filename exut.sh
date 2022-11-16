88a978a93b3d6f59a18929d96711f86ca11b0160
Ubuntu <ubuntu@ip-172-31-0-195.ap-south-1.compute.internal>
2022-11-16 05:54:37 +0000

    the git create add

---
+++ exut.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+new
+repo name
+descrption
+public
+ass readme
+
+copy https code
+git clone https
+cd
+go to folder
+ git branch
+ main
+create one dir
+cd dir
+create one file
+git add fname
+git commit -m "message"
+ git status
+ git push -u origin main
+ userneme
+ passwd
+
